51c95fe8814ffe0d735b058686e50adba8575770
Mohnish <mohnish.a007@gmail.com>
2026-04-21 22:35:30 +0100

    Longest subtring length using brute force approch

---
+++ dev_test/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message":"HelloWorld"}
+@app.get("/root1")
+def root1():
+    return {"message":"HelloWorldROOT1"}
